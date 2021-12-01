5a3bcdce32f0d6b96fe218cf94b64c364b04a909
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-01 14:06:47 -0300

    Creating the project with Spring Cloud Eureka Server

---
+++ src/main/java/com/romulopereira/Application.java
@@ -0,0 +1,14 @@
+package com.romulopereira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
